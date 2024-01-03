e5960d5b95e5c5ab3104af3daeab88e3a4092e4d
Yiqun (Ethan) Zhang <github@yzhang.io>
2024-01-03 11:51:09 -0800

    Check in tpc-ds_ordered

--- benchmarks/tpc-ds/tpc-ds_test.go
+++ benchmarks/tpc-ds/tpc-ds_test.go
@@ -9,20 +9,21 @@ import (
 )
 
 func TestGenOneFile(t *testing.T) {
+	t.SkipNow() // comment this to run.
 	queryFiles := make([]string, 0, 99)
 	for i := 1; i < 100; i++ {
 		queryFiles = append(queryFiles, fmt.Sprintf("query_%02d.sql", i))
 	}
-	stage := &stage.Stage{
+	s := &stage.Stage{
 		Id:         "serial",
 		Queries:    nil,
 		QueryFiles: queryFiles,
 	}
-	bytes, err := json.MarshalIndent(stage, "", "  ")
+	bytes, err := json.MarshalIndent(s, "", "  ")
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = os.WriteFile("serial.json", append(bytes, '\n'), 0644)
+	err = os.WriteFile("queries_serial.json", append(bytes, '\n'), 0644)
 	if err != nil {
 		t.Fatal(err)
 	}
