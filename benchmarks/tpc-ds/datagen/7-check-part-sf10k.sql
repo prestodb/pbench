7d09488e5e19cb145ffae5dd196d1af57cc94823
Yiqun (Ethan) Zhang <github@yzhang.io>
2024-03-07 03:04:40 -0600

    Add check number of partitions query

---
+++ benchmarks/tpc-ds/datagen/7-check-part-sf10k.sql
@@ -0,0 +1,7 @@
+select count(*) from "catalog_returns$partitions";
+select count(*) from "catalog_sales$partitions";
+select count(*) from "inventory$partitions";
+select count(*) from "store_returns$partitions";
+select count(*) from "store_sales$partitions";
+select count(*) from "web_returns$partitions";
+select count(*) from "web_sales$partitions";
---
+++ benchmarks/tpc-ds/datagen/7-check-part-sf1k.sql
@@ -0,0 +1,8 @@
+select count(*) from "catalog_sales$partitions";
+-- 1837
+select count(*) from "inventory$partitions";
+-- 261
+select count(*) from "store_sales$partitions";
+-- 1824
+select count(*) from "web_sales$partitions";
+-- 1824
