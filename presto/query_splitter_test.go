31d248c8b6555efe500435a963adb105ade27083
Yiqun (Ethan) Zhang <github@yzhang.io>
2024-03-09 21:06:32 -0600

    Add query splitter test for the new splitter behavior.

--- presto/query_splitter_test.go
+++ presto/query_splitter_test.go
@@ -1,24 +1,29 @@
 package presto_test
 
 import (
+	_ "embed"
 	"github.com/stretchr/testify/assert"
-	"presto-benchmark/presto"
+	"pbench/presto"
 	"strings"
 	"testing"
 )
+
+//go:embed btq_outer2inner.sql
+var fileWithTrailingComment string
 
 func TestQuerySplitter(t *testing.T) {
 	files := []string{
 		`select * from table1 where a = '\'--;'; --comment
 select * from table3;-;;
 `,
+		fileWithTrailingComment,
 	}
 	expectedQueries := [][]string{
 		{
 			"select * from table1 where a = '\\'--;'",
 			"--comment\nselect * from table3",
 			"-",
-		},
+		}, {fileWithTrailingComment[:663]},
 	}
 	for i, file := range files {
 		if queries, err := presto.SplitQueries(strings.NewReader(file)); err != nil {
