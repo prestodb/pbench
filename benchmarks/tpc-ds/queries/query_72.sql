0fa671eb3ee8107995583528d89b6e1dea87a2a0
Yiqun (Ethan) Zhang <github@yzhang.io>
2024-03-12 00:48:50 -0500

    keep 2 versions of q72

--- benchmarks/tpc-ds/queries/query_72.sql
+++ benchmarks/tpc-ds/queries/query_72.sql
@@ -1,31 +1,27 @@
 --TPC-DS Q72
-SELECT
-  i_item_desc
-, w_warehouse_name
-, d1.d_week_seq
-, sum((CASE WHEN (p_promo_sk IS NULL) THEN 1 ELSE 0 END)) no_promo
-, sum((CASE WHEN (p_promo_sk IS NOT NULL) THEN 1 ELSE 0 END)) promo
-, count(*) total_cnt
-FROM
-  ((((((((((catalog_sales
-INNER JOIN inventory ON (cs_item_sk = inv_item_sk))
-INNER JOIN warehouse ON (w_warehouse_sk = inv_warehouse_sk))
-INNER JOIN item ON (i_item_sk = cs_item_sk))
-INNER JOIN customer_demographics ON (cs_bill_cdemo_sk = cd_demo_sk))
-INNER JOIN household_demographics ON (cs_bill_hdemo_sk = hd_demo_sk))
-INNER JOIN date_dim d1 ON (cs_sold_date_sk = d1.d_date_sk))
-INNER JOIN date_dim d2 ON (inv_date_sk = d2.d_date_sk))
-INNER JOIN date_dim d3 ON (cs_ship_date_sk = d3.d_date_sk))
-LEFT JOIN promotion ON (cs_promo_sk = p_promo_sk))
-LEFT JOIN catalog_returns ON (cr_item_sk = cs_item_sk)
-   AND (cr_order_number = cs_order_number))
-WHERE (d1.d_week_seq = d2.d_week_seq)
-   AND (inv_quantity_on_hand < cs_quantity)
-   AND (d3.d_date > (d1.d_date + INTERVAL  '5' DAY))
-   AND (hd_buy_potential = '501-1000')
-   AND (d1.d_year = 1999)
-   AND (cd_marital_status = 'S')
-GROUP BY i_item_desc, w_warehouse_name, d1.d_week_seq
-ORDER BY total_cnt DESC, i_item_desc ASC, w_warehouse_name ASC, d1.d_week_seq ASC
-LIMIT 100
-
+select i_item_desc
+     , w_warehouse_name
+     , d1.d_week_seq
+     , sum(case when p_promo_sk is null then 1 else 0 end)     no_promo
+     , sum(case when p_promo_sk is not null then 1 else 0 end) promo
+     , count(*)                                                total_cnt
+from catalog_sales
+         join inventory on (cs_item_sk = inv_item_sk)
+         join warehouse on (w_warehouse_sk = inv_warehouse_sk)
+         join item on (i_item_sk = cs_item_sk)
+         join customer_demographics on (cs_bill_cdemo_sk = cd_demo_sk)
+         join household_demographics on (cs_bill_hdemo_sk = hd_demo_sk)
+         join date_dim d1 on (cs_sold_date_sk = d1.d_date_sk)
+         join date_dim d2 on (inv_date_sk = d2.d_date_sk)
+         join date_dim d3 on (cs_ship_date_sk = d3.d_date_sk)
+         left outer join promotion on (cs_promo_sk = p_promo_sk)
+         left outer join catalog_returns on (cr_item_sk = cs_item_sk and cr_order_number = cs_order_number)
+where d1.d_week_seq = d2.d_week_seq
+  and inv_quantity_on_hand < cs_quantity
+  and d3.d_date > date_add('day', 5, d1.d_date)
+  and hd_buy_potential = '501-1000'
+  and d1.d_year = 1999
+  and cd_marital_status = 'S'
+group by i_item_desc, w_warehouse_name, d1.d_week_seq
+order by total_cnt desc, i_item_desc, w_warehouse_name, d_week_seq
+limit 100;
