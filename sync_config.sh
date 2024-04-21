e74eb20a7f353b09b775c394e8b079d5373d5fea
Yiqun (Ethan) Zhang <github@yzhang.io>
2024-04-20 20:18:12 -0500

    Update timezone for appnexus

---
+++ sync_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cp -r $SCRIPT_DIR/cluster-configs/* $SCRIPT_DIR/../presto-performance/presto-deploy-cluster/clusters
+cp -r $SCRIPT_DIR/gen-config/templates $SCRIPT_DIR/../presto-performance/presto-deploy-cluster/clusters
--- upload.sh
+++ upload.sh
@@ -1,1 +1,4 @@
-aws s3 cp pbench_x86_64 s3://presto-deploy-infra-and-cluster-a9d5d14
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+aws s3 cp $SCRIPT_DIR/pbench_x86_64 s3://presto-deploy-infra-and-cluster-a9d5d14
