e1a96b6e6d0ad6377ebc78595a47cc063c5ab73d
Yiqun (Ethan) Zhang <github@yzhang.io>
2024-04-26 16:55:08 -0500

    Update clean_test_data.sql

--- loadjson/clean_test_data.sql
+++ loadjson/clean_test_data.sql
@@ -15,3 +15,10 @@ DELETE FROM presto_query_operator_stats WHERE query_id = '20240423_184422_00010_
 DELETE FROM presto_query_plans WHERE query_id = '20240423_184422_00010_3br7r_q88';
 DELETE FROM presto_query_stage_stats WHERE query_id = '20240423_184422_00010_3br7r_q88';
 DELETE FROM presto_query_statistics WHERE query_id = '20240423_184422_00010_3br7r_q88';
+
+DELETE FROM presto_query_creation_info WHERE query_id LIKE 'json041724-%';
+DELETE FROM presto_query_operator_stats WHERE query_id LIKE 'json041724-%';
+DELETE FROM presto_query_plans WHERE query_id LIKE 'json041724-%';
+DELETE FROM presto_query_stage_stats WHERE query_id LIKE 'json041724-%';
+DELETE FROM presto_query_statistics WHERE query_id LIKE 'json041724-%';
+
