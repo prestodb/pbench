a8effeb361b2287e7833bb59bbf1db9b06730bbc
Yiqun (Ethan) Zhang <github@yzhang.io>
2024-04-27 02:52:03 -0500

    Update test query json

--- loadjson/clean_test_data.sql
+++ loadjson/clean_test_data.sql
@@ -10,11 +10,11 @@ DELETE FROM presto_query_plans WHERE query_id = '20240423_081548_00000_rrtm7_err
 DELETE FROM presto_query_stage_stats WHERE query_id = '20240423_081548_00000_rrtm7_error';
 DELETE FROM presto_query_statistics WHERE query_id = '20240423_081548_00000_rrtm7_error';
 
-DELETE FROM presto_query_creation_info WHERE query_id = '20240423_184422_00010_3br7r_q88';
-DELETE FROM presto_query_operator_stats WHERE query_id = '20240423_184422_00010_3br7r_q88';
-DELETE FROM presto_query_plans WHERE query_id = '20240423_184422_00010_3br7r_q88';
-DELETE FROM presto_query_stage_stats WHERE query_id = '20240423_184422_00010_3br7r_q88';
-DELETE FROM presto_query_statistics WHERE query_id = '20240423_184422_00010_3br7r_q88';
+DELETE FROM presto_query_creation_info WHERE query_id = '20240427_073513_00020_5vbe8_q88';
+DELETE FROM presto_query_operator_stats WHERE query_id = '20240427_073513_00020_5vbe8_q88';
+DELETE FROM presto_query_plans WHERE query_id = '20240427_073513_00020_5vbe8_q88';
+DELETE FROM presto_query_stage_stats WHERE query_id = '20240427_073513_00020_5vbe8_q88';
+DELETE FROM presto_query_statistics WHERE query_id = '20240427_073513_00020_5vbe8_q88';
 
 DELETE FROM presto_query_creation_info WHERE query_id LIKE 'json041724%';
 DELETE FROM presto_query_operator_stats WHERE query_id LIKE 'json041724%';
