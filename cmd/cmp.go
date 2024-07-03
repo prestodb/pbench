71fa9296c3aaec934e65de64dfea00ca96c4ff93
Allen <allenshen@ibm.com>
2024-07-03 14:47:37 -0700

    Defined regular expression as a var before using it.

--- cmd/cmp.go
+++ cmd/cmp.go
@@ -13,8 +13,10 @@ var cmpCmd = &cobra.Command{
 	Short:                 "Compare two query result directories",
 }
 
+var defaultRegex = `.*(query_\d{2})(?:_c0)?\.output`
+
 func init() {
 	RootCmd.AddCommand(cmpCmd)
-	cmpCmd.Flags().StringVarP(&cmp.FileIdRegexStr, "file-id-regex", "r", `.*(query_\d{2})(?:_c0)?\.output`, "regex to extract file id from file names in two directories to find matching files to compare")
+	cmpCmd.Flags().StringVarP(&cmp.FileIdRegexStr, "file-id-regex", "r", defaultRegex, "regex to extract file id from file names in two directories to find matching files to compare")
 	cmpCmd.Flags().StringVarP(&cmp.OutputPath, "output-path", "o", "./diff", "diff output path")
 }
