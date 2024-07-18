f7d1d93467614851a237a9691f4de8a438f14102
Andrew X <andrewx.git@gmail.com>
2024-07-18 11:15:00 -0700

    Skip generating partitioned schemas on sf100 and below

--- cmd/genddl/main.go
+++ cmd/genddl/main.go
@@ -140,6 +140,11 @@ func generateSchemaFromDef(schema *Schema, defDir string, genDdlDir string, outp
 		return nil
 	})
 
+	// sf100 and below don't need any partitioned tables
+	if schema.Partitioned && schema.intScaleFactor() <= 100 {
+		return
+	}
+
 	generateCreateTable(schema, genDdlDir, outputDir, *step)
 	*step++
 
