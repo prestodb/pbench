cbad70baf4be052de27ad59547bc236b6cded848
xpengahana <120225775+xpengahana@users.noreply.github.com>
2024-11-13 17:04:04 -0600

    Genddl (#27)
    * Add sketch code
    
    * Add ddl generation (#11)
    
    * add genddl cmd
    * add generated examples
    * add analyze statement for unpartitioned hive schema
    
    ---------
    
    Co-authored-by: Xin Peng <xin@ahana.io>
    
    ---------
    
    Co-authored-by: Yiqun (Ethan) Zhang <github@yzhang.io>
    Co-authored-by: Archy-X <63976867+Archy-X@users.noreply.github.com>

---
+++ cmd/genddl/generated-examples/tpcds-sf1-parquet/6b-s3-mv-tpcds-sf1-parquet-partitioned-hive.sh
@@ -0,0 +1,4 @@
+aws s3 mv --recursive s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/catalog_sales/data/cs_sold_date_sk=null/ s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/catalog_sales/data/cs_sold_date_sk=__HIVE_DEFAULT_PARTITION__/
+aws s3 mv --recursive s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/inventory/data/inv_date_sk=null/ s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/inventory/data/inv_date_sk=__HIVE_DEFAULT_PARTITION__/
+aws s3 mv --recursive s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/store_sales/data/ss_sold_date_sk=null/ s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/store_sales/data/ss_sold_date_sk=__HIVE_DEFAULT_PARTITION__/
+aws s3 mv --recursive s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/web_sales/data/ws_sold_date_sk=null/ s3://presto-workload-v2/tpcds-sf1-parquet-partitioned-iceberg/web_sales/data/ws_sold_date_sk=__HIVE_DEFAULT_PARTITION__/
