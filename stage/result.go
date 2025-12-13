f690ceaf0f3648aa369fb78d7c84cc27bbcdcf44
Yabin Ma <mayabin@gmail.com>
2025-12-13 13:42:08 +0100

    fix data race issue

--- stage/result.go
+++ stage/result.go
@@ -1,42 +1,53 @@
 package stage
 
 import (
-	"github.com/rs/zerolog"
 	"pbench/log"
 	"time"
+
+	"github.com/rs/zerolog"
 )
 
 type QueryResult struct {
-	StageId       string
-	Query         *Query
-	QueryId       string
-	InfoUrl       string
-	QueryError    error
-	RowCount      int
-	StartTime     time.Time
-	EndTime       *time.Time
-	Duration      *time.Duration
-	simpleLogging bool
+	StageId    string
+	Query      *Query
+	QueryId    string
+	InfoUrl    string
+	QueryError error
+	RowCount   int
+	StartTime  time.Time
+	EndTime    *time.Time
+	Duration   *time.Duration
 }
 
-func (q *QueryResult) SimpleLogging() *QueryResult {
-	q.simpleLogging = true
-	return q
+// simpleQueryResult is a wrapper for logging QueryResult with reduced output
+type simpleQueryResult struct {
+	*QueryResult
+}
+
+func (q *QueryResult) SimpleLogging() zerolog.LogObjectMarshaler {
+	return simpleQueryResult{q}
 }
 
 func (q *QueryResult) MarshalZerologObject(e *zerolog.Event) {
+	q.marshalZerologObject(e, false)
+}
+
+func (s simpleQueryResult) MarshalZerologObject(e *zerolog.Event) {
+	s.QueryResult.marshalZerologObject(e, true)
+}
+
+func (q *QueryResult) marshalZerologObject(e *zerolog.Event, simpleLogging bool) {
 	e.Str("benchmark_stage_id", q.StageId)
 	if q.Query.File != nil {
 		e.Str("query_file", *q.Query.File)
-	} else if !q.simpleLogging {
+	} else if !simpleLogging {
 		e.Str("query", q.Query.Text)
 	}
 	e.Int("query_index", q.Query.Index)
 	e.Bool("cold_run", q.Query.ColdRun)
 	e.Int("sequence_no", q.Query.SequenceNo)
 	e.Str("info_url", q.InfoUrl)
-	if q.simpleLogging {
-		q.simpleLogging = false
+	if simpleLogging {
 		return
 	}
 	e.Str("query_id", q.QueryId)
