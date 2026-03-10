20d97d0dbedf37703f928c14477cea1507ce1c46
rpachipa <rpachipa@us.ibm.com>
2026-03-10 00:10:13 -0700

    Query refresh from tpcds spec 10TB POWER S0 run (#77)
    * spark queries
    
    * merge power s0 queries
    
    * delete a and b of multipart
    
    * remove spark queries
    
    * keep q14a and q14b as original
    
    * make q23,24 and q39 also multi-part
    
    * delete qall.sql

--- benchmarks/tpc-ds/queries/q43.sql
+++ benchmarks/tpc-ds/queries/q43.sql
@@ -1,6 +1,6 @@
---q43.sql--
+-- q43.sql
 
- select s_store_name, s_store_id,
+select  s_store_name, s_store_id,
         sum(case when (d_day_name='Sunday') then ss_sales_price else null end) sun_sales,
         sum(case when (d_day_name='Monday') then ss_sales_price else null end) mon_sales,
         sum(case when (d_day_name='Tuesday') then ss_sales_price else  null end) tue_sales,
@@ -11,10 +11,8 @@
  from date_dim, store_sales, store
  where d_date_sk = ss_sold_date_sk and
        s_store_sk = ss_store_sk and
-       s_gmt_offset = -5 and
-       d_year = 2000
+       s_gmt_offset = -7 and
+       d_year = 2002 
  group by s_store_name, s_store_id
- order by s_store_name, s_store_id,sun_sales,mon_sales,tue_sales,wed_sales,
-          thu_sales,fri_sales,sat_sales
- limit 100
-            
+ order by s_store_name, s_store_id,sun_sales,mon_sales,tue_sales,wed_sales,thu_sales,fri_sales,sat_sales
+ limit 100;
--- benchmarks/tpc-ds/queries/q86.sql
+++ benchmarks/tpc-ds/queries/q86.sql
@@ -1,15 +1,20 @@
---q86.sql--
+-- q86.sql
 
- select sum(ws_net_paid) as total_sum, i_category, i_class,
-  grouping(i_category)+grouping(i_class) as lochierarchy,
-  rank() over (
- 	    partition by grouping(i_category)+grouping(i_class),
- 	    case when grouping(i_class) = 0 then i_category end
- 	    order by sum(ws_net_paid) desc) as rank_within_parent
+select   
+    sum(ws_net_paid) as total_sum
+   ,i_category
+   ,i_class
+   ,grouping(i_category)+grouping(i_class) as lochierarchy
+   ,rank() over (
+ 	partition by grouping(i_category)+grouping(i_class),
+ 	case when grouping(i_class) = 0 then i_category end 
+ 	order by sum(ws_net_paid) desc) as rank_within_parent
  from
-    web_sales, date_dim d1, item
+    web_sales
+   ,date_dim       d1
+   ,item
  where
-    d1.d_month_seq between 1200 and 1200+11
+    d1.d_month_seq between 1182 and 1182+11
  and d1.d_date_sk = ws_sold_date_sk
  and i_item_sk  = ws_item_sk
  group by rollup(i_category,i_class)
@@ -17,5 +22,4 @@
    lochierarchy desc,
    case when lochierarchy = 0 then i_category end,
    rank_within_parent
- limit 100
-            
+ limit 100;
